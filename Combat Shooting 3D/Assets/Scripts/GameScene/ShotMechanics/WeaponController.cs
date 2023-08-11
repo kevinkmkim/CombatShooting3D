680a8bbc7c950d161900edf8f96160dc7b722436
Kevin Kim <kk946@cornell.edu>
2023-08-11 21:27:08 +0900

    Added smooth transition

--- Combat Shooting 3D/Assets/Scripts/GameScene/ShotMechanics/WeaponController.cs
+++ Combat Shooting 3D/Assets/Scripts/GameScene/ShotMechanics/WeaponController.cs
@@ -13,10 +13,14 @@ public class WeaponController : MonoBehaviour
 
     #region Properties
     private bool isAiming;
+    
     private Vector3 aimWeaponPosition = new Vector3(0.0f, -0.36f, 0.12f);
     private Quaternion aimWeaponRotation = Quaternion.identity;
     private Vector3 easeWeaponPosition = new Vector3(0.2f, -0.5f, 0.25f);
     private Quaternion easeWeaponRotation = Quaternion.Euler(12.0f, -3.5f, 15.0f);
+
+    private bool isWeaponMoving = false;
+    private float transitionDuration = 0.8f;
     #endregion
 
     private void OnEnable()
@@ -33,18 +37,49 @@ public class WeaponController : MonoBehaviour
 
     public void HandleAimEvent()
     {
-        Debug.Log("SHOOT from WeaponController");
+        Debug.Log("AIM from WeaponController");
         
-        isAiming = !isAiming;
-        if (isAiming)
+        if (!isWeaponMoving)
         {
-            weaponPivotTransform.localPosition = aimWeaponPosition;
-            weaponPivotTransform.localRotation = aimWeaponRotation;
+            isAiming = !isAiming;
+            if (isAiming)
+            {
+                StartCoroutine(MoveWeapon(aimWeaponPosition, aimWeaponRotation));
+            }
+            else
+            {
+                StartCoroutine(MoveWeapon(easeWeaponPosition, Quaternion.identity));
+            }
         }
-        else
+    }
+
+    private IEnumerator MoveWeapon(Vector3 newPosition, Quaternion newQuaternion)
+    {
+        isWeaponMoving = true;
+        float elapsedTime = 0.0f;
+        Vector3 initialPosition = weaponPivotTransform.localPosition;
+        Quaternion initialRotation = weaponPivotTransform.localRotation;
+
+        while (elapsedTime < transitionDuration)
         {
-            weaponPivotTransform.localPosition = easeWeaponPosition;
-            weaponPivotTransform.localRotation = Quaternion.identity;
+            float t = elapsedTime / transitionDuration;
+
+            float easedT = EaseInOutQuad(t);
+
+            weaponPivotTransform.localPosition = Vector3.Lerp(initialPosition, newPosition, easedT);
+            weaponPivotTransform.localRotation = Quaternion.Lerp(initialRotation, newQuaternion, easedT);
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
+
+        weaponPivotTransform.localPosition = newPosition;
+        weaponPivotTransform.localRotation = newQuaternion;
+        
+        isWeaponMoving = false;
+    }
+
+    private float EaseInOutQuad(float t)
+    {
+        return t < 0.5 ? 4 * Mathf.Pow(t, 3) : 1 - Mathf.Pow(-2 * t + 2, 3) / 2;
     }
 }
